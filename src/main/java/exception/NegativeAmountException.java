654b14ac3dc0ff79b622f7c5c85dc9a604d5bde9
Logan <logan.planche@gmail.com>
2020-10-07 11:55:48 +0200

    Converted project into Maven + added gitignore file + added java folder to src and test repo

--- src/main/java/exception/NegativeAmountException.java
+++ src/main/java/exception/NegativeAmountException.java
@@ -2,6 +2,8 @@ package exception;
 
 public class NegativeAmountException extends Exception{
 
+	private static final long serialVersionUID = 1L;
+	
     public NegativeAmountException() {
         super();
     }
